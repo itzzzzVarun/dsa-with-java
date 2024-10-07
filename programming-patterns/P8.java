1988fe8c75e90eff9be833a1949312ec479e77e2
ItzzzzVarun <itzzzzVarun>
2024-10-07 21:03:56 +0530

    learned loops and patterns

---
+++ programming-patterns/P8.java
@@ -0,0 +1,13 @@
+public class P8 {
+  public static void main(String[] args) {
+    int n = 5;
+    int num = 0;
+    for (int i = 1; i <= n; i++) {
+      for (int j = 1; j <= i; j++) {
+        num = num + 1;
+        System.out.print(num + " ");
+      }
+      System.out.println();
+    }
+  }
+}
